d32e6112d854ddaff37f1d02e40224da7652f871
FAHRI CIGDEM <64100513+fahricigdem@users.noreply.github.com>
2021-09-01 03:15:12 +0200

    container bg

--- src/components/Container.js
+++ src/components/Container.js
@@ -3,7 +3,7 @@ import { Flex, useColorMode } from '@chakra-ui/react'
 export const Container = (props) => {
   const { colorMode } = useColorMode()
 
-  const bgColor = { light: 'gray.300', dark: 'gray.900' }
+  const bgColor = { light: 'gray.500', dark: 'gray.900' }
 
   const color = { light: 'black', dark: 'white' }
   return (
